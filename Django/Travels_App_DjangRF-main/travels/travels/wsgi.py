8aaec5de7c58a56ef50a3cc77374cb6dd160dd04
Praveen Kumar <22a91a05b0@aec.edu.in>
2025-09-09 20:50:45 +0530

    Prepare project for Render deployment

--- Django/Travels_App_DjangRF-main/travels/travels/wsgi.py
+++ Django/Travels_App_DjangRF-main/travels/travels/wsgi.py
@@ -1,9 +1,5 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Add outer 'travels' folder to Python path
-sys.path.append("/opt/render/project/src/Django/Travels_App_DjangRF-main/travels")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travels.settings")
 
